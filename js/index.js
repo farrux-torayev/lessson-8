19971be0a60ed259e7109cdf5bf8b2c12af17fd5
farrux-torayev <farruxtorayev590@gmail.com>
2024-11-25 20:26:57 +0500

    lesson 8

---
+++ js/index.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      width: {
+        DEFAULT: "100%",
+        tablet: "600px",
+        largetablet: "600px",
+        desktop: "1200px",
+      },
+      padding: {
+        DEFAULT: "20px",
+      },
+    },
+    screens: {
+      tablet: "640px",
+      largetablet: "1000px",
+      desktop: "1240px",
+    },
+    extends: {
+      colors: {},
+    },
+  },
+};
